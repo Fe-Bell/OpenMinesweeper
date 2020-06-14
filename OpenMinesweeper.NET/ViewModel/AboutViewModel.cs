5ba66e3cc5acca1da5a1ec36383c6fcfc1208f5a
Felipe Bellini <Fe.Bell@outlook.com>
2020-06-14 14:35:56 -0400

    #4

--- OpenMinesweeper.NET/ViewModel/AboutViewModel.cs
+++ OpenMinesweeper.NET/ViewModel/AboutViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using GalaSoft.MvvmLight;
-using System;
-using System.Collections.Generic;
 using System.Reflection;
-using System.Text;
 
 namespace OpenMinesweeper.NET.ViewModel
 {
+    /// <summary>
+    /// Provides data for program information.
+    /// </summary>
     public class AboutViewModel : ViewModelBase
     {
+        /// <summary>
+        /// Returns name and version information for the about window.
+        /// </summary>
         public string ProductDetails
         {
             get => string.Format("OpenMinesweeper v{0}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
         }
 
+        /// <summary>
+        /// Creates a new instance of AboutViewModel.
+        /// </summary>
         public AboutViewModel()
         {
 
--- OpenMinesweeper.NET/ViewModel/ViewModelLocator.cs
+++ OpenMinesweeper.NET/ViewModel/ViewModelLocator.cs
@@ -1,19 +1,37 @@
 ﻿using GalaSoft.MvvmLight.Ioc;
 using OpenMinesweeper.Core;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace OpenMinesweeper.NET.ViewModel
 {
+    /// <summary>
+    /// Static class that provides static access to all viewmodels in the game.
+    /// </summary>
     public class ViewModelLocator
     {
+        /// <summary>
+        /// Returns an instance of the MinesweeperCore.
+        /// </summary>
         public static MinesweeperCore MinesweeperCore => SimpleIoc.Default.GetInstance<MinesweeperCore>();
+        /// <summary>
+        /// Returns an instance of the MainViewModel.
+        /// </summary>
         public static MainViewModel MainVM => SimpleIoc.Default.GetInstance<MainViewModel>();
+        /// <summary>
+        /// Returns an instance of the LoadGameStateViewModel.
+        /// </summary>
         public static LoadGameStateViewModel LoadGameStateVM => SimpleIoc.Default.GetInstance<LoadGameStateViewModel>();
+        /// <summary>
+        /// Returns an instance of the NewGameViewModel.
+        /// </summary>
         public static NewGameViewModel NewGameVM => SimpleIoc.Default.GetInstance<NewGameViewModel>();
+        /// <summary>
+        /// Returns an instance of the AboutViewModel.
+        /// </summary>
         public static AboutViewModel AboutVM => SimpleIoc.Default.GetInstance<AboutViewModel>();
 
+        /// <summary>
+        /// Static constructor.
+        /// </summary>
         static ViewModelLocator()
         {
             SimpleIoc.Default.Register<MinesweeperCore>();
